adc7f8b5a4d546b18e8c45419e365d50cd667200
ValtechInnovations <138601237+ValtechInnovations@users.noreply.github.com>
2023-12-05 11:16:04 +0530

    02_Commit
    New file index.js added and netlify.toml file updated

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,7 +4,12 @@ import serverless from "serverless-http";
 const api = express();
 
 const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+
+router.get("/hello", (req, res) => res.send("Hello User!"));
+
+router.get('/data', (req, res) => {
+    res.json({ message: 'This is a sample API route' });
+});
 
 api.use("/api/", router);
 
