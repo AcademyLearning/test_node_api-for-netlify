a7185307b98e65dce2604dbfde524a85c0d46a9a
ValtechInnovations <138601237+ValtechInnovations@users.noreply.github.com>
2023-12-08 09:30:02 +0530

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,23 +1,30 @@
-import express, { Router } from "express";
-import serverless from "serverless-http";
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
 
 const api = express();
 
 const router = Router();
 
+router.get('/', (req, res) => {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
-router.get("/", (req, res) => {
-    res.send("Hello User. API started => Try : /hello or /data");
+  res.send('Hello User. API started => Try : /hello or /data');
 });
 
-router.get("/hello", (req, res) => res.send("Hello User!"));
-
+router.get('/hello', (req, res) => res.send('Hello User!'));
 
 router.get('/data', (req, res) => {
-    res.json({ message: 'This is a sample API route' });
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+  res.json({ message: 'This is a sample API route' });
 });
 
-
-api.use("/", router);
+api.use('/', router);
 
 export const handler = serverless(api);
