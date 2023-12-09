3539e1be6fdcb6b9bd060ebc10f3fd1505a3c43b
ValtechInnovations <138601237+ValtechInnovations@users.noreply.github.com>
2023-12-09 08:51:02 +0530

    Revert "toml file update"
    This reverts commit fcb3f8f99cb8d1b548d07d588876ffe70b6cea94.

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,5 @@
-import express, { Router } from "express";
-import serverless from "serverless-http";
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
 
 const api = express();
 const router = Router();
@@ -9,19 +9,26 @@ api.use(cors({
     origin:"http://localhost:5173"
 }));
 
+router.get('/', (req, res) => {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
-router.get("/", (req, res) => {
-    res.send("Hello User. API started => Try : /hello or /data");
+  res.send('Hello User. API started => Try : /hello or /data');
 });
 
-router.get("/hello", (req, res) => res.send("Hello User!"));
-
+router.get('/hello', (req, res) => res.send('Hello User!'));
 
 router.get('/data', (req, res) => {
-    res.json({ message: 'This is a sample API route' });
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+  res.json({ message: 'This is a sample API route' });
 });
 
-
-api.use("/", router);
+api.use('/', router);
 
 export const handler = serverless(api);
