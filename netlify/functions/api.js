7073ce8f4286198e26a45bdbe5b1628a8bcb45cf
ValtechInnovations <138601237+ValtechInnovations@users.noreply.github.com>
2023-12-09 08:57:41 +0530

    Final update

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -10,22 +10,12 @@ api.use(cors({
 }));
 
 router.get('/', (req, res) => {
-  // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-
   res.send('Hello User. API started => Try : /hello or /data');
 });
 
 router.get('/hello', (req, res) => res.send('Hello User!'));
 
 router.get('/data', (req, res) => {
-  // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-
   res.json({ message: 'This is a sample API route' });
 });
 
